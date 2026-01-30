821a4dea89485ec8938a568daf0e260033202b0e
Tetiana Kubyshkina <tanyasha_@ukr.net>
2026-01-30 16:44:13 +0200

    upload form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,3 +1,39 @@
-console.log("Form");
+const storageKey = "feedback-form-state";
+
+let formData = {
+    email: "",
+    message: ""
+}
+
 const form = document.querySelector(".feedback-form");
-const textarea = form.querySelector(".message");
+form.addEventListener("submit", handleSubmit)
+form.addEventListener("input", onFormInput)
+populateForm(); 
+
+function onFormInput(event) {
+    const { name, value } = event.target;
+    if (name !== "email" && name !== "message") return;
+    formData[name] = value;
+    localStorage.setItem(storageKey, JSON.stringify(formData));
+}
+
+function populateForm() {
+    const saved = localStorage.getItem(storageKey)
+    if (!saved) return;
+    const parsed = JSON.parse(saved);
+    formData.email = parsed.email ?? "";
+    formData.message = parsed.message ?? "";
+    form.elements.email.value = formData.email;
+    form.elements.message.value = formData.message;
+}
+function handleSubmit(event) {
+    event.preventDefault();
+    if (formData.email.trim() === "" || formData.message.trim() === "") {
+        alert("Fill please all fields");
+        return;
+    }
+    localStorage.removeItem(storageKey);
+    formData.email = "";
+    formData.message = "";
+    event.currentTarget.reset();
+}
